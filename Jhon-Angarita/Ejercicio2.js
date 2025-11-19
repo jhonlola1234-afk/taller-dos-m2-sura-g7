8f96b24be2804576e973043e9aeac86ad5b7f386
DESKTOP-P4USMVD\Usuario <jhonlola1234@gmail.com>
2025-11-18 22:28:13 -0500

    Ejecicio 2

---
+++ Jhon-Angarita/Ejercicio2.js
@@ -0,0 +1,18 @@
+let usuarios = [
+  {
+    id: 1,
+    nombre: "Ana",
+  },
+  {
+    id: 2,
+    nombre: "Luis",
+  },
+  {
+    id: 3,
+    nombre: "Carlos",
+  },
+];
+
+let nombres = usuarios.map((usuario) => usuario.nombre);
+
+console.log(nombres);
