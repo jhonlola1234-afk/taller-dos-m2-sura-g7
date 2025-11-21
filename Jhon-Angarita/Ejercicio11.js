8a6975d9ff2b0bd6be6dc9043414b32595cf2d72
DESKTOP-P4USMVD\Usuario <jhonlola1234@gmail.com>
2025-11-20 22:36:39 -0500

    Ejecicio 11

---
+++ Jhon-Angarita/Ejercicio11.js
@@ -0,0 +1,8 @@
+let roles = ["USER", "EDITOR"];
+
+let tieneAdmin = roles.includes("ADMIN");
+let tieneEditor = roles.includes("EDITOR");
+
+console.log(roles);
+console.log(tieneAdmin);
+console.log(tieneEditor);
